e067510cc881d78f2be0133499348d2ebc6e158f
juanfer93 <jfpi_0793@hotmail.com>
2024-12-13 12:01:36 -0500

    cambios

--- backend/services/training.py
+++ backend/services/training.py
@@ -35,6 +35,7 @@ def train_model():
 
     print("Iniciando el entrenamiento...")
     print(f"Datos de entrada: {len(padded_sequences)} ejemplos, {len(tokenizer.word_index)} palabras.")
+
     history = model.fit(padded_sequences, np.array(labels_encoded), epochs=10, batch_size=16, verbose=2)
 
     print(f"\nEntrenamiento completado. Resultados finales: ")
